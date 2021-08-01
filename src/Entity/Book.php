79bf063a96d9eb74bcfc3a76cfd5384b6c8ca2b6
José Manuel Rodríguez Altamirano <omegaodin34@gmail.com>
2021-08-01 19:16:02 +0200

    Add book entity

---
+++ src/Entity/Book.php
@@ -0,0 +1,84 @@
+<?php
+
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Class Book
+ *
+ * @ORM\Entity()
+ * @ORM\Table(name="books")
+ *
+ * @package App\Entity
+ */
+class Book
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     *
+     * @var integer
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", nullable=false)
+     *
+     * @var string
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", nullable=false)
+     *
+     * @var string
+     */
+    private $description;
+
+    /**
+     * @return ?int
+     */
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return ?string
+     */
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     * @return Book
+     */
+    public function setName(string $name): Book
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    /**
+     * @return ?string
+     */
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    /**
+     * @param string $description
+     * @return Book
+     */
+    public function setDescription(string $description): Book
+    {
+        $this->description = $description;
+        return $this;
+    }
+}
