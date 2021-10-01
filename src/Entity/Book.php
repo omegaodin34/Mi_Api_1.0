ac4a8964ce6430f22baa46f444cc10ed852304e9
jose <omegaodin34@gmail.com>
2021-10-01 10:32:51 +0200

    "Endpoint create_book_by_id_name_description_with_POST complete"

--- src/Entity/Book.php
+++ src/Entity/Book.php
@@ -2,7 +2,6 @@
 
 
 namespace App\Entity;
-
 use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\ORM\Mapping as ORM;
 
@@ -25,21 +24,21 @@ class Book
      *
      * @var integer
      */
-    private $id;
+    private int $id;
 
     /**
      * @ORM\Column(type="string", nullable=false)
      *
      * @var string
      */
-    private $name;
+    private string $name;
 
     /**
      * @ORM\Column(type="string", nullable=false)
      *
      * @var string
      */
-    private $description;
+    private string $description;
 
     /**
      * @return ?int
